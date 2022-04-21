7d53e322888fb9428c3e317b9bb47c1dbadb5754
sam raphael <samraphael@gmail.com>
2022-04-21 08:17:33 -0400

    removed deprecated test

--- test/tests/middleware/test.js
+++ test/tests/middleware/test.js
@@ -1,11 +1,9 @@
 import app from '../../../src/server.js'
-import basicCRUD from './CRUD.js'
 import sessions from './sessions.js'
 
 let server
-describe('middleware', () => {
+describe('middleware etc', () => {
     before(() => server = app())
     after(() => server.close())
-    describe('basic CRUD routes', basicCRUD)
     describe('sessions', sessions)
 })
