b9c2e487976718f2bafe5ac0e7047533c380f6f5
sam raphael <samraphael@gmail.com>
2022-04-27 16:11:44 -0400

    small changes for validation,etc.

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,11 +1,18 @@
-import {authModel, usersModel} from "../../database/schema-files.js";
+import {authModel, criticsModel, usersModel} from "../../database/schema-files.js";
 
 export default async function signup(req, res) {
     const newUser = req.body;
+    if (!newUser.password || !newUser.userData.username){
+        res.status(403).send("missing data")
+        return
+    }
+
+
     const {   password , userData:{username,...rest}} = newUser
     let search = await authModel.find({username,password})
-    console.log(search)
-    if (search.length) {
+     if (search.length) {
+
+        console.log("Found User Auth")
         res.status(403).send(`User ${username} already exists!`)
         return
     }
@@ -22,19 +29,31 @@ export default async function signup(req, res) {
             password,
             user:user._id
         })
-        switch (rest.role) {
-            case"ADMIN":
-            case"NORMAL":
-                req.session.user = user._id
-                res.json(user)
-                return
-            case"CRITIC":
-                return
+        req.session.user = user._id
+        let crit
+        if (newUser.critic){
+            crit = newUser.critic
+        }else{
+            crit={experience:0,organisation:"NONE",}
         }
+
+
+        if (rest.role==="CRITIC"){
+            await criticsModel.create({user:user._id,...crit})
+        }
+
+        
+        console.log("USER: ",user)
+
+        res.json(user)
+
     }catch (e) {
+
         res.send(e.toString())
         return
     }
+
+
     res.sendStatus(400 )
 
 }
